a7f9ba4ca104ef0a82fea5ba3239ce07349b9cda
Lordfroze <yogaadipratama22@gmail.com>
2024-05-17 06:00:07 +0700

    semangat

--- javascript-projects/nodejs-web-server/server.js
+++ javascript-projects/nodejs-web-server/server.js
@@ -30,11 +30,31 @@ const requestListener = (request, response) => {
             // response bila client menggunakan GET
             response.end('<h1>Ini adalah homepage</h1>');
         } else{
-            //response bila client tidka mengggunakan GET
+            // response bila client tidka mengggunakan GET
             response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request </h1>`);
         }
     }else if(url === '/about'){
         // Todo 3: logika repson bila url bernilai '/about'
+            if(method === 'GET'){
+                // response bila client menggunakan GET
+                response.end('<h1>Halo! ini adalah halaman about</h1>')
+            } else if(method === 'POST'){
+                // response bile client menggunakan POST
+                let body = [];
+
+                request.on('data', (chunk) => {
+                    body.push(chunk);
+                });
+
+                request.on('end', () => {
+                    body =  Buffer.concat(body).toString();
+                    const {name} = JSON.parse(body);
+                    response.end(`<h1>Halo, ${name}! ini adalah halaman about</h1>`)
+                });
+            }else{
+                // response bile client tidak menggunakan GET ataupun POST
+                response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`)
+            }
     } else {
         // Todo 1: logika repsons bila url bukan '/' atau '/about'
         response.end('<h1>Halaman tidak ditemukan !</h1>');
